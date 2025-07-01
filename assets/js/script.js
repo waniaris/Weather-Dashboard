61381b005719d865920fcdcf5e0440aaff4a0ca9
waniaris <norhazwaniaris@gmail.com>
2025-07-01 21:10:09 +0100

    resolve conflict

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,6 +26,3 @@
 document.getElementById("getWeather").addEventListener("click", getCoordinates);
 
 
-// Call getWeather API when the button is clicked
-const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-
